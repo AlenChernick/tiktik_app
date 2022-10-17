09e7bdb73a92f2492ccff7688d3aba723bc009c0
AlenChernick <crazyguy771@gmail.com>
2022-10-17 18:29:11 +0300

    added user auth with google + navbar options + store using zustand

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ interface IProps {
 }
 
 const Home: NextPage<IProps> = ({ videos }) => {
-  console.log(videos);
   return (
     <div className='flex flex-col gap-10 videos h-full'>
       {videos.length ? videos.map((video: Video) => <VideoCard post={video} key={video._id} />) : <NoResults text={'No Videos'} />}
